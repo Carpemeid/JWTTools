f6008f72fa2f6e779d89b9081efdd349db62ece5
Dan Andoni <dan.andoni@bjss.com>
2016-08-21 18:58:04 +0100

    Initial Commit

---
+++ JWTTools/JWTTools.h
@@ -0,0 +1,19 @@
+//
+//  JWTTools.h
+//  JWTTools
+//
+//  Created by Andoni Dan on 21/08/16.
+//  Copyright © 2016 carpemeid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JWTTools.
+FOUNDATION_EXPORT double JWTToolsVersionNumber;
+
+//! Project version string for JWTTools.
+FOUNDATION_EXPORT const unsigned char JWTToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JWTTools/PublicHeader.h>
+
+
